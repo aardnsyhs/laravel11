9ea5dc60f88232899e81ff770e63f503ef04bcb4
aardnsyhs <ardiansyahdian755@gmail.com>
2024-06-27 10:47:35 +0700

    factory author has many post

--- database/migrations/2024_06_27_010340_create_posts_table.php
+++ database/migrations/2024_06_27_010340_create_posts_table.php
@@ -14,8 +14,10 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->unsignedBigInteger('author_id');
-            $table->foreign('author_id')->references('id')->on('users');
+            $table->foreignId('author_id')->constrained(
+                table: 'users',
+                indexName: 'posts_author_id'
+            );
             $table->string('slug')->unique();
             $table->text('body');
             $table->timestamps();
