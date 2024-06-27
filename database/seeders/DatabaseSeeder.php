755ca94625ae2d2fc1d0d3a7af5ab5749aeb5c76
aardnsyhs <ardiansyahdian755@gmail.com>
2024-06-27 16:37:22 +0700

    make CategorySeeder and UserSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +17,27 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        // Category::create([
+        //     'name' => 'Web Design',
+        //     'slug' => 'web-design'
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul Artikel 1',
+        //     'author_id' => '1',
+        //     'category_id' => '1',
+        //     'slug' => 'judul-artikel-1',
+        //     'body' => 'lorem ipsum dolor sit amet, consectetur adip incididunt ut labore et dolore magna aliqu Lorem ipsum dolor sit amet lore mauris vel aug commod incididunt ut labore et dolore magna aliqu Lorem ipsum dolor sit amet lore mauris vel aug commod incididunt ut labore et dolore magna aliqu Lorem ipsum dolor sit amet lore mauris vel aug'
+        // ]);
+        $this->call([CategorySeeder::class, UserSeeder::class]);
+        Post::factory(100)->recycle([
+            Category::all(),
+            User::all()
+        ])->create();
     }
 }
