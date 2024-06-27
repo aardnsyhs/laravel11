54526bf978d9491bb29ce86fdfdc29d51a7425ee
aardnsyhs <ardiansyahdian755@gmail.com>
2024-06-27 08:04:54 +0700

    make model + migration

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,7 +3,7 @@
     <article class="py-4 max-w-screen-md">
         <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
         <div class="text-base text-gray-500">
-            <a href="#">{{ $post['author'] }}</a> | 26 June 2023
+            <a href="#">{{ $post['author'] }}</a> | {{ $post->created_at->diffForHumans() }}
         </div>
         <p class="my-4 font-light">{{ $post['body'] }}</p>
         <a href="/posts" class="font-medium text-blue-500 hover:underline">&laquo; Back To Post</a>
