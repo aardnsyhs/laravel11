b83cf4db5c528039cdfc7a468ec2a1c16970f5d4
aardnsyhs <ardiansyahdian755@gmail.com>
2024-06-27 11:09:06 +0700

    make ui for author has many post

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,6 +13,10 @@ Route::get('/posts', function () {
 
 Route::get('/posts/{post:slug}', function(Post $post) {
         return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
+Route::get('/authors/{user}', function(User $user) {
+    return view('posts', ['title' => 'Article by ' . $user->name, 'posts' => $user->posts]);
 });
 
 Route::get('/about', function () {
